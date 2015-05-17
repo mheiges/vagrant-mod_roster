f24f784078446bb8ecd1e3648a211a5270d73c82
Mark Heiges <mheiges@uga.edu>
2015-05-17 16:53:36 -0400

    add Puppet directives to disable ipv6 because YUM sometimes tries to use ipv6, but leave commented out because I am not sure it is needed.

--- puppet/manifests/default.pp
+++ puppet/manifests/default.pp
@@ -3,3 +3,21 @@ Package {
 }
 
 class { '::firewall_impl': }
+
+#exec { "reload_sysctl":
+#  command => "/sbin/sysctl -e -p",
+#  refreshonly => true;
+#}
+#
+#augeas { 'net.ipv6.conf.all.disable_ipv6':
+#  context => "/files/etc/sysctl.conf",
+#  changes => ["set net.ipv6.conf.all.disable_ipv6 1"],
+#  notify  => Exec["reload_sysctl"],
+#}
+#
+#augeas { 'net.ipv6.conf.default.disable_ipv6':
+#  context => "/files/etc/sysctl.conf",
+#  changes => ["set net.ipv6.conf.default.disable_ipv6 1"],
+#  require => Augeas['net.ipv6.conf.all.disable_ipv6'],
+#  notify  => Exec["reload_sysctl"],
+#}
